ad01920cc6304ccfeebe1b5ce16d507df65b00a5
Eudson Souza <eudson.souza@outlook.com>
2025-10-20 02:42:12 -0300

    refactor(domain): improve business logic and entities

--- src/ProductCatalog.Domain/Entities/Color.cs
+++ src/ProductCatalog.Domain/Entities/Color.cs
@@ -8,7 +8,7 @@ public class Color
     public bool IsActive { get; set; } = true;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-    
+
     // Navigation Properties
     public ICollection<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
 }
